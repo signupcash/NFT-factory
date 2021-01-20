bff9cd1fc0d185cdeccb6f1ffa8a56f6ef8981e0
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2021-01-21 01:06:12 +0600

    footer ui updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,54 +1,83 @@
 import React, { Component } from 'react';
-import { Link } from 'theme-ui';
+import { 
+  Box,
+  Image,
+  Flex, 
+  Grid, 
+  Link,
+  ThemeProvider
+} from 'theme-ui';
+import theme from './theme';
+import logo from '../images/signup-logo.png';
 
 export class Footer extends Component {
   render() {
     return (
-      <footer sx={{
-        fontSize: 1,
-        variant: 'styles.footer',
-        p: 4,
-        bg: 'muted',
-      }}>
-        <div sx={{
-          display: 'grid',
-          gridTemplateRows: 'repeat(4, 32px)',
-          gridTemplateColumns: [
-            'repeat(2, 1fr)',
-            'repeat(4, 1fr)'
-          ],
-          gridAutoFlow: 'column',
-          px: 2,
-          py: 4,
-          m: 4
+      <ThemeProvider theme={theme}>
+        <Flex 
+          bg='primary'
+          sx={{
+          fontSize: 1,
+          variant: 'styles.footer',
+          px: 4,
+          pb: 4,
+          backgroundColor: '#e5e7eb',
+          display: 'flex',
+          flexWrap: 'wrap',
+          alignItems: 'center'
         }}>
-          <Link to='/' sx={{
-            variant: 'styles.navlink', 
-            p: 2,
-            py: 2,
-            px: 4,
-            fontFamily: '"Poppins", sans-serif',
-            fontSize: '30px',
-            fontWeight: 'bold',
-            color: '#6d66FF'
-          }}
+          <Grid
+            gap={'10vw'}
+            columns={[ 2, null, 4 ]}
+            sx={{
+              display: 'grid',
+              m: 4,
+              mx: 'auto',
+              px: 'auto',
+            }}
           >
-            signup
-          </Link>
-          <Link to='/' sx={{ variant: 'styles.navlink', p: 2 }}>GETTING STARTED</Link>
-          <Link to='/' sx={{ variant: 'styles.navlink', p: 2 }}>Documentation</Link>
-          <Link to='/' sx={{ variant: 'styles.navlink', p: 2 }}>Demos</Link>
-          <Link to='/' sx={{ variant: 'styles.navlink', p: 2 }}>Intro Blog Post</Link>
-          <Link to='/' sx={{ variant: 'styles.navlink', p: 2 }}>CORE</Link>
-          <Link to='/' sx={{ variant: 'styles.navlink', p: 2 }}>Wallet</Link>
-          <Link to='/' sx={{ variant: 'styles.navlink', p: 2 }}>Contact</Link>
-          <Link to='/' sx={{ variant: 'styles.navlink', p: 2 }}>COMMUNITY</Link>
-          <Link to='/' sx={{ variant: 'styles.navlink', p: 2 }}>Github</Link>
-          <Link to='/' sx={{ variant: 'styles.navlink', p: 2 }}>Twitter</Link>
-          <Link to='/' sx={{ variant: 'styles.navlink', p: 2 }}>Telegram</Link>
-          <Link to='/' sx={{ variant: 'styles.navlink', p: 2 }}>ReadCash</Link>
-        </div>
-      </footer>
+            <Box my={4}>
+              <Link to='/' sx={{
+                alignItems: 'left',
+                mx: 4,
+              }}
+              >
+                <Image width={'50%'} src={logo}/>
+              </Link>
+            </Box>
+            <Box sx={{
+              display: 'grid',
+              gridTemplateRows: 'repeat(4, 32px)',
+              gridAutoFlow: 'column'
+            }}>
+              <Link to='/' sx={{ variant: 'styles.navlink', p: 2 }}><b>GETTING STARTED</b></Link>
+              <Link to='/' sx={{ variant: 'styles.navlink', p: 2 }}>Documentation</Link>
+              <Link to='/' sx={{ variant: 'styles.navlink', p: 2 }}>Demos</Link>
+              <Link to='/' sx={{ variant: 'styles.navlink', p: 2 }}>Intro Blog Post</Link>
+            </Box>
+            <Box sx={{
+              display: 'grid',
+              gridTemplateRows: 'repeat(3, 32px)',
+              gridAutoFlow: 'column'
+            }}>
+              <Link to='/' sx={{ variant: 'styles.navlink', p: 2 }}><b>CORE</b></Link>
+              <Link to='/' sx={{ variant: 'styles.navlink', p: 2 }}>Wallet</Link>
+              <Link to='/' sx={{ variant: 'styles.navlink', p: 2 }}>Contact</Link>
+            </Box>
+            <Box sx={{
+              display: 'grid',
+              gridTemplateRows: 'repeat(5, 32px)',
+              gridAutoFlow: 'column'
+            }}>
+              <Link to='/' sx={{ variant: 'styles.navlink', p: 2 }}><b>COMMUNITY</b></Link>
+              <Link to='/' sx={{ variant: 'styles.navlink', p: 2 }}>Github</Link>
+              <Link to='/' sx={{ variant: 'styles.navlink', p: 2 }}>Twitter</Link>
+              <Link to='/' sx={{ variant: 'styles.navlink', p: 2 }}>Telegram</Link>
+              <Link to='/' sx={{ variant: 'styles.navlink', p: 2 }}>ReadCash</Link>
+            </Box>
+          </Grid>
+        </Flex>
+      </ThemeProvider>
     )
   }
 }
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,86 +3,83 @@ import {
   Button,
   Container,
   Divider,
+  Image,
   Heading,
   Link,
-  ThemeProvider 
+  ThemeProvider,
 } from 'theme-ui';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import theme from './theme';
+import logo from '../images/signup-logo.png';
 
 export class Layout extends Component {
   render() {
     return (
       <ThemeProvider theme={theme}>
-        <Container p={4} bg='muted' sx={{
-          flex: 1,
-          justifyContent: 'flex-end'
-        }}>
+        <Container p={4} bg='muted'>
           <Heading sx={{
-            mx: 'auto',
-            alignItems: 'center'
+            alignItems: 'center',
+            display: 'flex'
           }}>
-            <Link to='/' 
+            <Link href='https://signup.cash/' 
               sx={{
                 py: 2,
                 px: 4,
-                fontFamily: '"Poppins", sans-serif',
-                fontSize: '30px',
-                fontWeight: 'bold',
-                color: '#6d66FF'
               }}
             >
-              signup
+              <Image width={100} src={logo} />
             </Link>
-            <Button
-              to='/!'
-              sx={{
-                fontSize: '17px',
-                appearance: 'none',
-                display: 'inline-block',
-                textAlign: 'center',
-                lineHeight: 'inherit',
-                textDecoration: 'none',
-                fontFamily: 'inherit',
-                flex: 1,
-                m: 0,
-                px: 3,
-                py: 2,
-                border: 0,
-                borderRadius: 8,
-                variant: 'buttons.primary',
-              }}
-            >
-                Wallet
-            </Button>
-            <Link href='https://docs.signup.cash/'
-              sx={{
-                fontSize: '17px',
-                color: 'grey',
-                fontFamily: 'inherit',
-                textDecoration: 'none',
-                fontWeight: 400,
-                letterSpacing: '0.03em',
-                m: 0,
-                px: 4,
-                py: 2
-              }}
-            >
-                Documentation
-            </Link>
-            <Link href='https://github.com/signupcash'
-              sx={{
-                fontSize: '30px',
-                color: 'grey',
-                m: 0,
-                px: 4,
-                py: 2,
-              }}
-            >
-              <FontAwesomeIcon icon={faGithub} />
-            </Link>
-            <div />
+            <div sx={{ mx: 'auto' }} />
+            <div sx={{ position: 'relative', float: 'right' }}>
+              <Button
+                to='/!'
+                sx={{
+                  fontSize: '17px',
+                  appearance: 'none',
+                  display: 'inline',
+                  textAlign: 'center',
+                  lineHeight: 'inherit',
+                  textDecoration: 'none',
+                  fontFamily: 'inherit',
+                  flex: 1,
+                  m: 0,
+                  px: 3,
+                  py: 2,
+                  border: 0,
+                  borderRadius: 8,
+                  variant: 'buttons.primary',
+                }}
+              >
+                  Wallet
+              </Button>
+              <Link href='https://docs.signup.cash/'
+                sx={{
+                  fontSize: '17px',
+                  color: 'grey',
+                  fontFamily: 'inherit',
+                  textDecoration: 'none',
+                  fontWeight: 400,
+                  letterSpacing: '0.03em',
+                  m: 0,
+                  px: 4,
+                  py: 2
+                }}
+              >
+                  Documentation
+              </Link>
+              <Link href='https://github.com/signupcash'
+                sx={{
+                  fontSize: '30px',
+                  color: 'grey',
+                  m: 0,
+                  px: 4,
+                  py: 2,
+                }}
+              >
+                <FontAwesomeIcon icon={faGithub} />
+              </Link>
+            </div>
           </Heading>
           <Divider sx={{
             color: 'grey',
