9c5f648cdf3f5bc05b2459b9fecb0247b92c552f
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2021-01-26 01:42:04 +0600

    ui update v2

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import './App.css';
 import Container from './components/Container';
-import Layout from './components/Layout';
-import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
       <Route path="/" component={Container} />
-
     </div>
   )
 }
